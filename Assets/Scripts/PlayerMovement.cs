1667dfd7abd082f72a49705603f7367b438cf4a2
Aditya Sharma <aditya.0302.sharma@gmail.com>
2023-05-21 15:16:12 +0530

    Robot Movement Bug fixes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Transactions;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Photon.Realtime;
+
 
 [RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
 public class PlayerMovement : MonoBehaviour
@@ -21,18 +23,31 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody m_RigidBody;
     private CapsuleCollider m_CapsuleCollider;
     private bool m_Grounded = true;
+    private float m_JumpInitHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         m_RigidBody = GetComponent<Rigidbody>();
         m_CapsuleCollider = GetComponent<CapsuleCollider>();
+
+        m_JumpInitHeight = transform.position.y;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+
+        // if (transform.position.y > m_JumpInitHeight + 0.5)
+        // {
+        //     direction = Vector3.zero;
+        // }
+        // if (transform.position.y == m_JumpInitHeight)
+        // {
+        //     m_RigidBody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        // }
+
 
         if (direction == Vector3.zero)
         {
@@ -42,7 +57,9 @@ public class PlayerMovement : MonoBehaviour
         {
             robotAnimator.enabled = true;
         }
-        transform.Translate(direction * speed * Time.fixedDeltaTime);
+        // m_RigidBody.useGravity = false;
+        transform.Translate(speed * Time.fixedDeltaTime * direction);
+        // m_RigidBody.useGravity = true;
         // transform.rotation = Quaternion.LookRotation(direction);
 
         m_Grounded = Physics.Raycast(transform.position, Vector3.down, m_CapsuleCollider.bounds.extents.y + groundCheckDistance, groundLayer);
@@ -53,6 +70,8 @@ public class PlayerMovement : MonoBehaviour
     {
         if(m_Grounded)
         {
+            // m_RigidBody.constraints = ~RigidbodyConstraints.FreezePositionY;
+            // transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
             m_RigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
